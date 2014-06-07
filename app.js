e9045bc1df28b3d5b27b1d4595d0b955790f8df2
Ted Hille <tedhille@gmail.com>
2014-06-07 10:14:56 -0700

    have express log to stderr so as not to give control back to grunt (and thusly exit)

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  // write to stderr or grunt regains control and exits...
+  app.use(express.logger({ format: 'dev', stream: process.stderr }));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -31,5 +32,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  // console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
