ce73918891e86894e598a72edde026ff1a6060c7
Ted Hille <ehille@appnexus.cmo>
2014-06-11 23:06:42 -0400

    fix tests that broke last night

--- test/midi-visualizer-pipeline.mspec.js
+++ test/midi-visualizer-pipeline.mspec.js
@@ -92,7 +92,6 @@ describe('MidiRenderPipeline', function () {
 
       it('should leave the classname of the DOM node to "off"', function () {
          mockElements.forEach(function (element) {
-            console.log('CLASSNAME: ' + element.className);
             element.className.should.match(/off/);
          });
       });
