92e151d7f370b1b02ae5f30d1da8ceb0c5974574
Ted Hille <ehille@appnexus.cmo>
2014-06-13 22:02:42 -0400

    add beginning test for midi-visualizer

---
+++ test/midi-visualizer.mspec.js
@@ -0,0 +1,87 @@
+/* vim: set expandtab ts=3 sw=3: */
+/* jshint expr: true */
+/* globals describe: true, beforeEach: true, afterEach: true, it: true, Promise: true, xit: true */
+
+'use strict';
+
+var chai = require('chai'),
+    sinon = require('sinon'),
+    utils = require('../lib/utils.js'),
+    AbstractRenderer = require('../lib/midi-visualizer/renderer/abstract.js'),
+    MidiVisualizer = require('../lib/midi-visualizer.js');
+
+describe('midi-visualizer', function () {
+   var expect = chai.expect,
+      MockRenderer;
+
+
+	chai.should();
+
+   beforeEach(function () {
+      MockRenderer = function MockRenderer() {};
+      MockRenderer.prototype = Object.create(AbstractRenderer.prototype);
+      MockRenderer.prototype.render = sinon.spy();
+      MockRenderer.prototype.filters = { testFilter: sinon.stub() };
+   });
+
+	describe('error conditions', function () {
+
+		it('should throw error when no params', function () {
+			expect(function () {
+				new MidiVisualizer();
+			}).to.throw(/config/);
+		});
+
+		it('should throw error when no config for midi-pipeline-renderer', function () {
+			expect(function () {
+				new MidiVisualizer({});
+			}).to.throw(/config/);
+		});
+	});
+
+	describe('default construction', function () {
+		var midiVisualizer;
+
+		beforeEach(function () {
+			midiVisualizer = new MidiVisualizer({
+				config: {
+					renderer: MockRenderer,
+					filters: [MockRenderer.prototype.filters.testFilter]
+				}
+			});
+		});
+
+		it('should have start offset of zero', function () {
+			midiVisualizer.startOffsetMs.should.equal(0);
+		});
+	});
+
+   describe('api', function () {
+      var midiVisualizer;
+
+		beforeEach(function () {
+			midiVisualizer = new MidiVisualizer({
+				config: {
+					renderer: MockRenderer,
+					filters: [MockRenderer.prototype.filters.testFilter]
+				}
+			});
+		});
+      
+      describe('#setStage', function () {
+         var spyCallback,
+            promise;
+
+         beforeEach(function () {
+            spyCallback = sinon.spy();
+
+            // TODO: can't do Promises in node...
+            // promise = midiVisualizer.setStage(spyCallback);
+         });
+
+         xit('should return a promise', function () {
+            promise.should.be.instanceof(Promise);
+         });
+      });
+   });
+});
