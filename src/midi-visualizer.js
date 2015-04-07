2431dc0f9eeb68f6fa61617bfe463c77a9ea38da
Ted Hille <tedhille@gmail.com>
2015-04-06 23:01:46 -0700

    added transformation step to convert midi data into anim data

--- src/midi-visualizer.js
+++ src/midi-visualizer.js
@@ -4,6 +4,100 @@
 
 var utils = require('funtils');
 var monad = utils.monad;
+var types = require('./data-types');
+var AnimEvent = types.AnimEvent;
+var midiParser = require('func-midi-parser');
+var MidiNoteEvent = midiParser.types.MidiNoteEvent;
+var MidiNoteOnEvent = midiParser.types.MidiNoteOnEvent;
+var MidiNoteOffEvent = midiParser.types.MidiNoteOffEvent;
+var MidiMetaTempoEvent = midiParser.types.MidiMetaTempoEvent;
+
+function trackEventFilter(event) {
+	return event instanceof MidiNoteEvent || event instanceof MidiMetaTempoEvent;
+}
+
+function transformMidi(midi) {
+	var tempo = 500000; // default of 120bpm
+	var tickInMicroSec = tempo / midi.header.timeDivision;
+
+	return midi.tracks.reduce(function _reduceTrack(eventsByTime, track, trackIndex) {
+		var elapsedTimeInMicroSec = 0;
+		var activeNotes = {};
+
+		return track.events.reduce(function _reduceEvent(eventsByTime, event) {
+			var eventTimeInMs = 0;
+			var startTimeMs = 0;
+			var eventLength = 0;
+			var startNote = {};
+			var newEvent = {};
+
+			if (event instanceof MidiMetaTempoEvent) {
+				// NOTE: this "should" be the first event in a track if not, 
+				//       we would really need to go back and revise the time for all events...
+				tempo = event.tempo;
+				tickInMicroSec = Math.floor(tempo / midi.header.timeDivision);
+
+				return eventsByTime;
+			}
+
+			elapsedTimeInMicroSec += event.delta * tickInMicroSec;
+			eventTimeInMs = Math.floor(elapsedTimeInMicroSec / 1000);
+
+			/* istanbul ignore else */
+			if (!trackEventFilter(event)) return eventsByTime;
+
+			if (event instanceof MidiNoteOnEvent) {
+				// start tracking a note "start"
+				activeNotes[event.note] = activeNotes[event.note] || [];
+				activeNotes[event.note].push({
+					event: event,
+					startTimeMicroSec: elapsedTimeInMicroSec,
+					index: 0 // assume we are the first note for this time-slice
+				});
+			} else if (event instanceof MidiNoteOffEvent) {
+				startNote = activeNotes[event.note] ? activeNotes[event.note][0] : null;
+
+				if (startNote) {
+					startNote.length = elapsedTimeInMicroSec - startNote.startTimeMicroSec;
+					startTimeMs = Math.floor(startNote.startTimeMicroSec / 1000);
+					newEvent = new AnimEvent({
+						event: startNote.event,
+						length: startNote.length,
+						track: trackIndex
+					});
+
+					eventsByTime[startTimeMs][startNote.index] = newEvent;
+					activeNotes[event.note].pop();
+
+					/* istanbul ignore else */
+					if (activeNotes[event.note].length === 0) delete activeNotes[event.note];
+				} else {
+					console.error('no active note "' + event.note + '", track "' + trackIndex + '"');
+
+					return eventsByTime;
+				}
+			}
+
+			/* istanbul ignore else */
+			if (!activeNotes[event.note] || activeNotes[event.note].length <= 1) {
+				eventsByTime[eventTimeInMs] = eventsByTime[eventTimeInMs] || [];
+				eventsByTime[eventTimeInMs].push(new AnimEvent({
+					event: event,
+					length: eventLength,
+					track: trackIndex
+				}));
+
+				/* istanbul ignore else */
+				if (activeNotes[event.note]) {
+					// set the index of the last active note such that it will be added after the note we just added
+					activeNotes[event.note][activeNotes[event.note].length - 1].index = eventsByTime[eventTimeInMs].length - 1;
+				}
+			}
+
+			return eventsByTime;
+		}, eventsByTime);
+	}, {});
+}
 
 function playVisualizer(state) {
 	state.audioPlayer.play();
@@ -14,10 +108,20 @@ function playVisualizer(state) {
 	});
 }
 
+function prepVisualizer(state) {
+	var nextState = state.next({
+		animEventsByTimeMs: transformMidi(state.midi)
+	});
+
+	return nextState;
+}
+
 var visualizer = monad();
+visualizer.lift('prep', prepVisualizer);
 visualizer.lift('play', playVisualizer);
 
 module.exports = {
 	visualizer: visualizer,
 	types: require('./data-types')
 };
+
--- test/midi-visualizer.spec.js
+++ test/midi-visualizer.spec.js
@@ -1,72 +1,132 @@
+/* jshint expr: true */
+/* globals describe: true, beforeEach: true, afterEach: true, it: true */
 'use strict';
 
 var chai = require('chai');
 var expect = chai.expect;
 var sinon = require('sinon');
 
-var AudioPlayer = require('../src/audio-player');
+var midiParser = require('func-midi-parser');
+var MidiNoteOnEvent = midiParser.types.MidiNoteOnEvent;
+var MidiNoteOffEvent = midiParser.types.MidiNoteOffEvent;
+var MidiMetaTempoEvent = midiParser.types.MidiMetaTempoEvent;
 
-var midiVisualizer  = require('../src/midi-visualizer');
+var midiVisualizer = require('../src/midi-visualizer');
 var visualizer = midiVisualizer.visualizer;
 var MidiVisualizerState = midiVisualizer.types.MidiVisualizerState;
 
-describe('midi-visualizer', function () {
-	var midiVisualizer, audioPlayerStub, rendererStub;
+describe('midi-visualizer', function() {
 
-	beforeEach(function (done) {
-		audioPlayerStub = sinon.stub({
-			play: function () {},
-			getPlayheadTime: function () {}
+	describe('#prep', function() {
+		var state, midiData, midiVisualizer;
+
+		beforeEach(function(done) {
+			midiData = {
+				header: {
+					// TODO: make this a number to give us nice, round test values
+					timeDivision: 1000
+				},
+				tracks: [{
+					events: [new MidiMetaTempoEvent({
+						delta: 0,
+						dataBytes: [0xFF, 0xF], // TODO: make this a number to give us nice, round test values
+						subtype: 'tempo'
+					}), new MidiNoteOnEvent({
+						note: 1,
+						delta: 0
+					}), new MidiNoteOffEvent({
+						note: 1,
+						delta: 1000
+					})]
+				}, {
+					// TODO: add a non-note, non-tempo event to exercise that branch
+					events: [new MidiNoteOnEvent({
+						note: 2,
+						delta: 100
+					}), new MidiNoteOffEvent({
+						note: 2,
+						delta: 2000
+					})]
+				}]
+			};
+
+			midiVisualizer = visualizer(new MidiVisualizerState({
+				midi: midiData
+			}));
+
+			midiVisualizer = midiVisualizer.prep();
+			state = midiVisualizer.value();
+
+			done();
 		});
 
-		rendererStub = sinon.stub({
-			scheduleAnimation: function () {}			
+		afterEach(function(done) {
+			state = midiData = midiVisualizer = null;
+			done();
 		});
 
-		midiVisualizer = visualizer(new MidiVisualizerState({
-			audioPlayer: audioPlayerStub,
-			renderer: rendererStub
-		}));
+		it('should have converted midi data into animEvents by time', function(done) {
+			// TODO: get the math right to line these up nicely...
+			expect(Object.keys(state.animEventsByTimeMs)).to.eql(['0', '6', '65', '136']);
+			done();
+		});
 
-		done();
+		it('should have calculated length of each note');
+
+		it('should log an error for seeing an end note with no begginging, but still parse');
 	});
 
-	afterEach(function (done) {
-		midiVisualizer = audioPlayerStub = rendererStub = null;
+	describe('#play', function() {
+		var state, midiVisualizer, audioPlayerStub, rendererStub;
 
-		done();
-	});
+		beforeEach(function(done) {
+			audioPlayerStub = sinon.stub({
+				play: function() {},
+				getPlayheadTime: function() {}
+			});
 
-	describe('#play', function () {
-		var state;
+			rendererStub = sinon.stub({
+				scheduleAnimation: function() {}
+			});
 
-		beforeEach(function (done) {
+			midiVisualizer = visualizer(new MidiVisualizerState({
+				audioPlayer: audioPlayerStub,
+				renderer: rendererStub,
+				midi: {}
+			}));
+
 			midiVisualizer = midiVisualizer.play();
 			state = midiVisualizer.value();
 
 			done();
 		});
 
-		it('should set the state to playing', function (done) {
+		afterEach(function(done) {
+			state = midiVisualizer = audioPlayerStub = rendererStub = null;
+			done();
+		});
+
+		it('should set the state to playing', function(done) {
 			expect(state.isPlaying).to.be.true;
 			done();
 		});
 
-		it('should start the audioPlayer', function (done) {
+		it('should start the audioPlayer', function(done) {
 			expect(audioPlayerStub.play.called).to.be.true;
 
 			done();
 		});
 
-		it('should start get the audioPlayer playhead time', function (done) {
+		it('should start get the audioPlayer playhead time', function(done) {
 			expect(audioPlayerStub.getPlayheadTime.called).to.be.true;
 
 			done();
 		});
 
-		it('should ask renderer to schedule animations', function (done) {
+		it('should ask renderer to schedule animations', function(done) {
 			expect(rendererStub.scheduleAnimation.called).to.be.true;
 			done();
 		});
 	});
 });
+
