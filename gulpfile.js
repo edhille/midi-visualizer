a8e11ad333f28e17b6a26dd3990809c59045201c
Ted Hille <tedhille@gmail.com>
2016-02-21 11:28:44 -0800

    updated deps and cleaned up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,13 @@
 
 var gulp = require('gulp');
 
-// var minimist = require('minimist');
-// var knownOptions = [
-// 	{ string: 'html-output', 'default': false }
-// ];
-// var options = minimist(process.argv.slice(2), knownOptions);
-
 function clean(cb) {
 	var del = require('del');
 
-	del([
-		'./dist/*',
-		'./coverage/*'
-	], cb);
-}
-
-function build() {
-	var browserify = require('browserify');
-	var through2 = require('through2');
-	var rename = require('gulp-rename');
-
-    return gulp.src('./src/main.js')
-        .pipe(through2.obj(function (file, enc, next) {
-            browserify(file.path, { debug: process.env.NODE_ENV === 'development' })
-                .bundle(function (err, res) {
-                    if (err) { return next(err); }
- 
-                    file.contents = res;
-                    next(null, file);
-                });
-        }))
-        .on('error', function (error) {
-            console.log(error.stack);
-            this.emit('end');
-        })
-        .pipe(rename('bundle.js'))
-        .pipe(gulp.dest('./dist'));
+	del(['./coverage/*']).then(function (paths) {
+		/* eslint no-console: [0] */
+		console.log('Deleted:\n', paths.join('\n'));
+	}).then(cb);
 }
 
 function test(cb, doCoverage) {
@@ -67,8 +38,5 @@ function watch() {
 }
 
 gulp.task('clean', clean);
-gulp.task('build', ['clean'], build);
-gulp.task('test', ['clean'], test);
 gulp.task('coverage', ['clean'], coverage);
 gulp.task('watch', watch);
-gulp.task('default', ['build']);
